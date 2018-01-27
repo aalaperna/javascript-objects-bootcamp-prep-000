85fa09dba5e05c9d3bc0d23b61c1daa8a5ab6308
Alyssa LaPerna <aalaperna@gmail.com>
2018-01-27 18:52:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
 var playlist = { artistName: "songTitle" };
 
-function updatePlaylist (playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
+function updatePlaylist (object, key, value) {
+ object[key] = value
   return playlist
 }
 
-function removeFromPlaylist (playlist, artistName) {
-  delete playlist[artistName]
+function removeFromPlaylist (object, key) {
+  delete object[key]
   return playlist
 }
